59f73d5d4428194b0bcea974af4588f4b42f0ad5
natively.dev <api@natively.dev>
2026-05-18 04:00:48 +0000

    map styling and interaction changes

--- app/map-view.tsx
+++ app/map-view.tsx
@@ -93,7 +93,14 @@ function buildMapHtml(
 <body>
   <div id="map"></div>
   <script>
-    var map = L.map('map', { zoomControl: false }).setView([${centerLat}, ${centerLng}], ${initialZoom});
+    var map = L.map('map', {
+  zoomControl: false,
+  dragging: true,        // false = locked, no panning
+  scrollWheelZoom: true, // false = disable scroll zoom
+  doubleClickZoom: true, // false = disable double-tap zoom
+  minZoom: 2,            // how far out user can zoom
+  maxZoom: 19,           // how far in user can zoom
+}).setView([${centerLat}, ${centerLng}], ${initialZoom});
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       maxZoom: 19,
