99056c97e6d0ff10434eeaad7d039b3d50e71bd6
natively.dev <api@natively.dev>
2026-05-19 10:19:08 +0000

    fix: Prevent react-native-maps codegen crash on web

--- metro.config.js
+++ metro.config.js
@@ -273,4 +273,11 @@ config.server.enhanceMiddleware = (middleware) => {
   };
 };
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-maps') {
+    return { type: 'empty' };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
