bbf2b0ad7313cac21c753da485594b52eb6a87e6
natively.dev <api@natively.dev>
2026-05-19 11:09:01 +0000

    fix: Disable EAS cache and rewrite OTEL_PKG import

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      './babel-plugins/strip-otel-dynamic-import.js',
       [
         "module-resolver",
         {
