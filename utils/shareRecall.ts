fb667f247950a4003b192a9d8f6517f3ae5a4183
natively.dev <api@natively.dev>
2026-05-23 06:29:31 +0000

    shorten and put URL on same line

--- utils/shareRecall.ts
+++ utils/shareRecall.ts
@@ -64,7 +64,7 @@ export async function shareRecall(recall: Note, currentImageIndex: number = 0, o
 
     // Extracted URL(s) on a labeled line so iOS Messages parses each as its own link
     if (extractedUrls.length > 0) {
-      shareMessage += '🔗 Link:\n';
+      shareMessage += '🔗 ';
       for (const url of extractedUrls) {
         shareMessage += `${url}\n`;
       }
